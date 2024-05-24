5c1a7cf3d21d7ffd561e7d58a2ac5633ddf4cdf8
Rafael Tiribás <rafaeltiribas@outlook.com>
2024-05-24 16:01:48 -0300

    .

---
+++ alerta_dengue/android/app/src/main/kotlin/com/example/alerta_dengue/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.alerta_dengue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ alerta_dengue/lib/pages/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  _buildAppBar() {
+    return AppBar(
+      title: Text('Alerta Dengue'),
+      centerTitle: true,
+    );
+  }
+
+  _buildBody() {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [],
+      ),
+    );
+  }
+}
+
